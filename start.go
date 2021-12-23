dcf16497a75f215eb25e78db529e35ab03361a49
Linda A. Stanford <90364136+Buttires1984@users.noreply.github.com>
2021-12-23 23:16:00 +0800

    Update start.go

--- start.go
+++ start.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"runtime/trace"
 )
 
 func main() {
-	var mem runtime.MemStats
+	// create file
+	f, err := os.Create("/tmp/traceFile.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
+	err = trace.Start(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer trace.Stop()
+
+	for i := 0; i < 10; i++ {
+		s := make([]byte, 50000000)
+		if s == nil {
+			fmt.Println("Operation failed")
+		}
+	}
+
+	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
 
 	// print mem stats with alloc, totalalloc, heapalloc, numgc
+	ReadMemStats(&mem)
+}
+
+func ReadMemStats(mem *runtime.MemStats) {
 	fmt.Println("mem.Alloc:", mem.Alloc)
 	fmt.Println("mem.TotalAlloc:", mem.TotalAlloc)
 	fmt.Println("mem.HeapAlloc:", mem.HeapAlloc)
